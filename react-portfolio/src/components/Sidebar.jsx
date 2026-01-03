cfebb3d53d4f0d0f13dc1019cc882ca92a30032c
Priti Saxena <saxenapriti1008@gmail.com>
2026-01-03 13:24:28 +0100

    updated sidebar.jsx
    updated skills

--- react-portfolio/src/components/Sidebar.jsx
+++ react-portfolio/src/components/Sidebar.jsx
@@ -13,8 +13,8 @@ function Sidebar() {
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#education">Education</a></li>
+         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#skills">Skills</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
